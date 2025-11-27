bb536f72fe877c68c498f4b6035abeeb99a1ad6a
Krasimir <141431815+KNedov@users.noreply.github.com>
2025-11-27 13:40:00 +0200

    Add home page layout and routing with components
    Introduces Home, ProductCard, and NoPhoneMsg components for the home page, along with associated CSS for styling. Sets up React Router for navigation, updates App.jsx to include header, footer, and routes, and applies global styles. Adds Font Awesome for icons and updates dependencies to include react-router.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
-
+import { Route, Routes } from 'react-router'
 import './App.css'
+import Footer from './components/footer/Footer.jsx'
+import Header from './components/header/Header.jsx'
+import Home from './components/home/Home.jsx'
+
 
 function App() {
 
   return (
     <>
+      <Header/>
+      <Routes>
+        <Route path="/" element={<Home />} />
       
+      </Routes>
+      <Footer/>
     </>
   )
 }
